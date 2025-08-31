29072afb23270319d135dfb0031d091886ca9b46
Amanda <amanda.asgm@gmail.com>
2025-08-31 13:40:56 -0300

    correcting name of the table courses

--- src/database/migrations/20250830193822_create-courses.ts
+++ src/database/migrations/20250830193822_create-courses.ts
@@ -4,7 +4,7 @@ import type { Knex } from "knex";
 // 5. 
 export async function up(knex: Knex): Promise<void> {
   // 7. creating the table
-  await knex.schema.createTable("course", (table) => {
+  await knex.schema.createTable("courses", (table) => {
     // 7.1 creating the colunms of the table
     table.increments("id").primary(),
     table.text('name').notNullable(),
@@ -16,6 +16,6 @@ export async function up(knex: Knex): Promise<void> {
 // 6.
 export async function down(knex: Knex): Promise<void> {
   // 8. undo table courses
-  await knex.schema.dropTable("course")
+  await knex.schema.dropTable("courses")
 }
 
--- src/database/migrations/20250831162622_add-updated-courses.ts
+++ src/database/migrations/20250831162622_add-updated-courses.ts
@@ -2,14 +2,14 @@ import type { Knex } from "knex";
 
 // 9. new column updated_at
 export async function up(knex: Knex): Promise<void> {
-  await knex.schema.alterTable('course', (table) => {
+  await knex.schema.alterTable('courses', (table) => {
     table.timestamp("updated_at").defaultTo(knex.fn.now()).after("created_at")
   })
 }
 
-// 9.1 
+// 9.1 undo the action created on "up"
 export async function down(knex: Knex): Promise<void> {
-await knex.schema.alterTable('course', (table) => {
+await knex.schema.alterTable('courses', (table) => {
     table.dropColumn("updated_at")
   })}
 
